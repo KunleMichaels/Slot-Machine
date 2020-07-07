d1483888146763519bd0118a4ed94fe8c751c848
Kunle Michaels Kolawole <KunleMichaels@Michaels-MacBook-Pro.local>
2020-07-07 15:09:35 +0100

    upgrade to webpack 4, new css plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require("path");
 const webpack = require("webpack");
 const js = path.resolve(__dirname, "js");
 const modules = path.resolve(__dirname, "js/components");
-const extractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./js/main.js",
   output: {
     filename: "./js/app.js",
@@ -12,11 +13,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
         test: /\.(s*)css$/,
-        use: extractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader", "sass-loader"],
-        }),
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
@@ -32,14 +37,8 @@ module.exports = {
         ],
       },
     ],
-    loaders: [
-      {
-        loader: "babel-loader",
-      },
-    ],
   },
-  plugins: [new extractTextPlugin({ filename: "app.bundle.css" })],
+  plugins: [new MiniCssExtractPlugin({ filename: "app.bundle.css" })],
   devtool: "#inline-source-map",
-
   watch: true,
 };
